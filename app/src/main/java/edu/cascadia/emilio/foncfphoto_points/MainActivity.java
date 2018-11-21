f9d18f15f60af7c7498baa16b0a14b49b6f49f55
Aaron <aaronakohler@gmail.com>
2018-11-20 18:15:17 -0800

    initial commit for issue 3

--- app/src/main/java/edu/cascadia/emilio/foncfphoto_points/MainActivity.java
+++ app/src/main/java/edu/cascadia/emilio/foncfphoto_points/MainActivity.java
@@ -1,7 +1,15 @@
 package edu.cascadia.emilio.foncfphoto_points;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.hello);
+
+        Activity contact = new ContactActivity();
+        Intent intent = new Intent(this, ContactActivity.class);
+        startActivity(intent);
+
+
     }
 }
