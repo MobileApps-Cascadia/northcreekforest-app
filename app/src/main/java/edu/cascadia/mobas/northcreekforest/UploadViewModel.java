02d12c8316e2f6e2ed096c7f0694afa572cf3bec
Cascadia-EmilioDiaz <xenorai@gmail.com>
2019-03-19 16:34:10 -0700

    Chris-Sprint3 into Develop.

--- app/src/main/java/edu/cascadia/mobas/northcreekforest/UploadViewModel.java
+++ app/src/main/java/edu/cascadia/mobas/northcreekforest/UploadViewModel.java
@@ -1,8 +1,8 @@
 package edu.cascadia.mobas.northcreekforest;
 
 import android.app.Application;
-import android.arch.lifecycle.AndroidViewModel;
-import android.support.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.annotation.NonNull;
 
 public class UploadViewModel extends AndroidViewModel {
     //private <REPOSITORY> repository;
